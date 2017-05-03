6e417ba86f32604119f6d42a781d046192f07954
eugeneross <e@eugeneross.com>
2017-05-02 23:58:04 -0400

    init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>It Works!</h1>
+        <p>Testing <span className="redBg">Numie</span> Frame work.</p>
+        <p>Enjoy!</p>
+      </div>
+    )
+  }
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const loaders = require('./config/webpack.loaders');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+loaders.push({
+  test: /\.sass$/,
+  loader: ExtractTextPlugin.extract({fallback: 'style-loader', use : 'css-loader?sourceMap&localIdentName=[local]___[hash:base64:5]!sass-loader?outputStyle=expanded'}),
+  exclude: ['node_modules']
+});
+
+module.exports = {
+  entry: [
+    './src/index.jsx',
+    './src/styles/index.sass'
+  ],
+  output: {
+    publicPath: './',
+    path: path.join(__dirname, 'public'),
+    filename: '[chunkhash].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders
+  },
+  plugins: [
+    new WebpackCleanupPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        drop_console: true,
+        drop_debugger: true
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      files: {
+        css: ['style.css'],
+        js: ['main.js'],
+      }
+    })
+  ]
+};
