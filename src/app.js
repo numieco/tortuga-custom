5c9dcbf9da09116e67a10bcc14f9c4b8414867bb
eugeneross <e@eugeneross.com>
2017-05-09 15:33:11 -0400

    index.html properties

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import {Helmet} from "react-helmet";
-import '../styles/index.scss';
-=======
 import './styles/index.sass';
->>>>>>> 5ac25c230bc853eb2a78ef5baee735334baf4f70
 
 export default class App extends React.Component {
   render() {
