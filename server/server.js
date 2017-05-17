f891d4475a81fb19784c066048fe0e7076ff781c
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-05-18 02:31:58 +0530

    Revert "Change project according to tortuga framework."
    This reverts commit 0069bd36bdde306cc1c3e5cb861d9c7fbd2591da.

--- server/server.js
+++ server/server.js
@@ -1,36 +1,16 @@
 const express = require('express')
 const path = require('path')
 
-const request = require('request')
-const ghostSecret = require('../src/ghost-secret')
-
 const app = express()
 const host = process.env.HOST || "localhost"
 const port = process.env.PORT || 8000
 
 app.use(express.static(path.resolve(__dirname, "..", "public")))
 
-app.get('/all', (req, res) => {
-
-  request.get('https://all-american-gold.ghost.io/ghost/api/v0.1/posts/?limit=all&client_id='+ ghostSecret.clientId +'&client_secret='+ ghostSecret.clientSecret
-    , (err, response, body) => {
-      if (!err && response.statusCode == 200) {
-        res.send(response.body)
-      }
-    }
-  )
-})
-
-app.get('/slug/*', (req, res) => {
-
-  request.get('https://all-american-gold.ghost.io/ghost/api/v0.1/posts/?limit=all&client_id='+ ghostSecret.clientId +'&client_secret='+ ghostSecret.clientSecret
-    , (err, response, body) => {
-      if (!err && response.statusCode == 200) {
-        res.send(response.body)
-      }
-    }
-  )
-})
+/*
+  All routes which are defined in React app
+  will be handled at client-side (using react-router).
+*/
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "public", "index.html"))
--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import './styles/index.sass';
-
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-
-import App from './containers/App'
-import IndividualArticlePage from './containers/IndividualArticlePage'
-import ArchivePosts from './containers/ArchivePosts'
-import AboutPage from './components/AboutPage'
-import BlogPage from './components/BlogPage'
-import MetalsPage from './components/MetalsPage'
-import ShopPage from './components/ShopPage'
-import ContactPage from './components/ContactPage'
+import App from './app';
 
 render(
-  <Router>
-    <div>
-      <Route exact path='/' component={ App } />
-      <Route path='/post/' component={ IndividualArticlePage } />
-      <Route path='/page/about' component={ AboutPage } />
-      <Route path='/page/blog' component={ BlogPage } />
-      <Route path='/page/metals-program' component={ MetalsPage } />
-      <Route path='/page/shop' component={ ShopPage } />
-      <Route path='/page/contact' component={ ContactPage } />
-      <Route path='/archive/' component={ ArchivePosts } />
-    </div>
-  </Router>
-  , document.getElementById('app')
-)
+  <App/>,
+  document.querySelector("#app")
+);
+
+if (module && module.hot) {
+  module.hot.accept('./app.js', () => {
+    const App = require('./app.js').default;
+    render(
+        <App/>,
+      document.querySelector("#app")
+    );
+  });
+}
